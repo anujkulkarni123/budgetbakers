3ad62462784d82498fc5c1c4659b72d3772d76cb
Kyle Tan <k46tan@uwaterloo.ca>
2024-05-08 15:38:45 -0400

    added placeholder AdminService for admin functions

---
+++ src/main/java/com/exavalu/services/AdminService.java
@@ -0,0 +1,19 @@
+package com.exavalu.services;
+
+import com.exavalu.entities.Category;
+import com.exavalu.pojos.PropertyValues;
+
+public class AdminService {
+	public static boolean updateCategories(Category category, PropertyValues propertyValues) {
+		return false;
+	}
+	public static boolean updateCurrency(String currencyName, double currencyExchangeRate, PropertyValues propertyValues) {
+		return false;
+	}
+	public static boolean addCategories(Category category, PropertyValues propertyValues) {
+		return false;
+	}
+	public static boolean addCurrency(String currencyName, double currencyExchangeRate, PropertyValues propertyValues) {
+		return false;
+	}
+}
